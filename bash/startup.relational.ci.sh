a1e0d9b0f4f3f746ac4de51bcc746e02011768ae
Maham Sheikh <maham.sheikh@conradlabs.com>
2024-10-24 12:32:55 +0500

    build: hostname

--- bash/startup.relational.ci.sh
+++ bash/startup.relational.ci.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 set -e
 
-/opt/wait-for-it.sh postgres:5432
+/opt/wait-for-it.sh "${DATABASE_HOST}:${DATABASE_PORT:-5432}"
 npm run migration:run
 npm run seed:run:relational
 npm run start:prod > prod.log 2>&1 &
--- bash/startup.relational.test.sh
+++ bash/startup.relational.test.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 set -e
 
-/opt/wait-for-it.sh postgres:5432
+/opt/wait-for-it.sh "${DATABASE_HOST}:${DATABASE_PORT:-5432}"
 /opt/wait-for-it.sh maildev:1080
 npm install
 npm run migration:run
